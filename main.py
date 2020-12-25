b7b9cf7a86279d521aee2a0174019f3708bc0fe4
dMahra <54589314+dMahra@users.noreply.github.com>
2020-12-24 17:26:06 -0800

    added new pathname for image after moving numberrecognition into local GitHub directory.

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import cv2 as cv
 from utils import *
 
 
-pathImage = '/Users/darsh_mahra/PycharmProjects/numberrecognition/media/secondhand.jpg'
+# pathImage = '/Users/darsh_mahra/PycharmProjects/numberrecognition/media/secondhand.jpg'
+pathImage = '/Users/darsh_mahra/Documents/GitHub/SudokuSolver/numberrecognition/media/secondhand.jpg'
+
+
 heightimage, widthimage = 450, 450
 Model = initializeModel()
 
